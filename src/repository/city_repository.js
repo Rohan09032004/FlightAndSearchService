ac24628552af4c1bd78748d16c139ff4308895dd
Rohan Aggarwal <rohanaggarwal090204@gmail.com>
2025-07-15 20:37:34 +0530

    completed city repository

--- src/repository/city_repository.js
+++ src/repository/city_repository.js
@@ -3,7 +3,9 @@ const {City} = require('../models/index.js')
 class CityRepository{
     async createCity({name}){
         try{
-            const city = await City.create({name});
+            const city = await City.create({
+                name
+            });
             return city;
         }
         catch(error){
@@ -25,5 +27,39 @@ class CityRepository{
             throw {error};
         }
     }
+    async updateCity(cityID, data){
+        try{
+            // The Below approach also works but will not return the updated object.
+            // if we are using postgress then returning:true can be used,else not
+            // const city = await City.update(data, {
+            //     where :{
+            //         id :cityID
+            //     },
+            //     returning :true
+            // });
+
+            // for getting updated data in mysql, we use the below approach
+            const city = await City.findByPk(cityID);
+            city.name = data.name;
+            await city.save();
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repository Layer.");
+            throw {error};
+        }
+
+    }
+
+    async getCity(cityid){
+        try{
+            const city = await City.findByPk(cityid);
+            return city;
+        }
+        catch (error){
+            console.log("Something went wrong in the repository Layer.");
+            throw {error};
+        }
+    }
+
 };
 module.exports = CityRepository;
